19e8a7ca572d3cd236b00981f2cb9248ddda7063
fofisop <sophiasakhnovsky@gmail.com>
2026-07-14 15:52:26 -0400

    maybe I'll be done soon, this is confusing but also cool i added animations and cool stuff

--- travelll/sketch.js
+++ travelll/sketch.js
@@ -1,7 +1,8 @@
+1
 let eyeWid =50;
 let eyeHig = 40;
 let pupilWid = 22;
-let pupilHih =25;
+let pupilHig =25;
 
 
 
@@ -12,26 +13,59 @@ function setup() {
 
     createCanvas(800, 600);
 
-  background(255,249,226); 
+   background('#87CEFA'); 
 }
+
 function draw() {
+  strokeWeight(1);
     background(220);
     angleMode(DEGREES);
     rectMode(CENTER);
-    ellipse(200,200,175,200);
+    fill(0, 255, 255);
+
+   ellipse(400,300,175,200);
+  
+  
 
 
 
 
     //eyesinthese
-      ellipse(170, 170, eyeWid, eyeHig);
+   
+if (mouseIsPressed) {
+  fill(0);
+  ellipse(370, 280, eyeWid, eyeHig / 8);
+  ellipse(430, 280, eyeWid, eyeHig / 8);
+} else {
+  fill('#B2EBF2');
+  ellipse(370, 280, eyeWid, eyeHig);
+  ellipse(430, 280, eyeWid, eyeHig);
 
-  ellipse(230, 170, eyeWid, eyeHig);
-  ellipse(170, 170, pupilWid, pupilHig);
-  ellipse( 230, 170, pupilWid, pupilHig);
-
+  fill('#000000');
+  ellipse(370, 280, pupilWid, pupilHig);
+  ellipse(430, 280, pupilWid, pupilHig);
+}
+  
 
   //restofcode
-  arc (200, 230, 50, 50, 0, 180);
+  fill('#FF7F50'); 
+ if (mouseIsPressed) {
+  fill('#FF7F50'); 
+  arc(400, 330, 50, 50, 0, 180);
+} else {
+  fill(0, 255, 255); 
+  arc(400, 330, 50, 50, 0, 180);
+}
+
+
+  strokeWeight(5);
+  line(312, 320, 240, 360);  
+  line(488, 320, 560, 360); 
+  line(370, 395, 370, 480); 
+
+
+  line(430, 395, 430, 480); 
+ 
+   text("Click to blink!", width - 10, height - 10);
 
 }
